1a41a48503d68e549af0967ef1fe449a80d92cf2
AUTUMN <krshlee22@naver.com>
2023-01-27 23:59:35 +0900

    2023-01-27

--- level1/문자열 내 마음대로 정렬하기/answer.js
+++ level1/문자열 내 마음대로 정렬하기/answer.js
@@ -1,7 +1,23 @@
 function solution(strings, n) {
-    var answer = [];
-    return answer;
+    let str = ''
+    let answer = strings.sort((str1, str2) => {
+        let a = str1[n]
+        let b = str2[n]
+        if (a > b) return 1
+        else if(a < b) return -1
+        else str = a
+    });
+    let restArr = answer.filter(data => data[n] == str).sort()
+    console.log(restArr, answer)
+    //return answer;
 }
 
-console.log(solution(["sun", "bed", "car"], 1))
+function solution2(strings, n) {
+    return strings.sort()
+}
+
+// console.log(solution(["sun", "bed", "car"], 1))
 console.log(solution(["abce", "abcd", "cdx"], 2))
+// console.log(solution(["zbcrw", "cdbwb", "sccer"], 2))
+console.log(solution(["abce", "abcd", "abcdx"], 2))
+// console.log(solution2(["abce", "abcd", "abcdx"], 2))
