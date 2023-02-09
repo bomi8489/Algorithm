f44c761a6e552edaabe0f1ee6e0f12c7efea287d
AUTUMN <krshlee22@naver.com>
2023-02-09 18:43:27 +0900

    2023-02-09

--- level1/콜라 문제/answer.js
+++ level1/콜라 문제/answer.js
@@ -1,7 +1,12 @@
 function solution(a, b, n) {
-    var answer = 0;
-    return answer;
+    let change = 0
+    while(n >= a) {
+        change += Math.floor(n/a)*b
+        n = Math.floor(n/a)*b + n%a
+    }
+    return change
 }
 
 console.log(solution(2, 1, 20))
 console.log(solution(3, 1, 20))
+console.log(solution(3, 2, 10))
