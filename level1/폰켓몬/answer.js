53f4ea4fcb9c2ed6ad54a02b8a8934428d9ab523
AUTUMN <krshlee22@naver.com>
2023-02-11 23:37:30 +0900

    2023-02-11

--- level1/폰켓몬/answer.js
+++ level1/폰켓몬/answer.js
@@ -1,4 +1,9 @@
 function solution(nums) {
-    var answer = 0;
-    return answer;
+    let numsLength = nums.length
+    let setNums = [...new Set(nums)];
+    return (numsLength/2 < setNums.length) ? answer = Math.floor(numsLength/2) : answer = setNums.length
 }
+
+console.log(solution([3, 1, 2, 3]))
+console.log(solution([3, 3, 3, 2, 2, 4]))
+console.log(solution([3, 3, 3, 2, 2, 2]))
