d3badce24ee44d544351a34382d5caa45dcd4d3e
AUTUMN <krshlee22@naver.com>
2023-04-27 18:49:38 +0900

    추억점수

--- level1/추억점수/answer.js
+++ level1/추억점수/answer.js
@@ -1,22 +1,24 @@
-// function solution(name, yearning, photo) {
-//     let answer = Array(photo.length).fill(0);
-//     photo.map(photoArray => 
-//         photoArray.map(person => {
-//             if(name.findIndex((personName) => personName === person) !== -1) {
-//                 return name.findIndex((personName) => personName === person)
-//             }
-//         }).filter(data => data !== undefined)
-//     ).map((indexArray, idx) => 
-//         indexArray.map(nameIndex => 
-//             answer[idx] += yearning[nameIndex]
-//         )
-//     )
-//     return answer;
-// }
-
 function solution(name, yearning, photo) {
-    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0))
+    let answer = Array(photo.length).fill(0);
+    photo.map(photoArray => 
+        photoArray.map(person => {
+            if(name.findIndex((personName) => personName === person) !== -1) {
+                return name.findIndex((personName) => personName === person)
+            }
+        }).filter(data => data !== undefined)
+    ).map((indexArray, idx) => 
+        indexArray.map(nameIndex => 
+            answer[idx] += yearning[nameIndex]
+        )
+    )
+    return answer;
 }
+
+// function solution(name, yearning, photo) {
+//     return photo.map(photoArray=>
+//         photoArray.reduce((acc, cur)=> acc += yearning[name.indexOf(cur)] ?? 0, 0)
+//     )
+// }
 
 console.log(solution(
     ["may", "kein", "kain", "radi"], 
