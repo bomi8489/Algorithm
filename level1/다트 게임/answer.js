a407722e5b935134ec25747a0e0dd53d01ef603a
AUTUMN <krshlee22@naver.com>
2023-04-28 17:11:32 +0900

    다트 게임

---
+++ level1/다트 게임/answer.js
@@ -0,0 +1,26 @@
+function solution(dartResult) {
+    const bonus = { 'S': 1, 'D': 2, 'T': 3 },
+          options = { '*': 2, '#': -1, undefined: 1 };
+
+    let darts = dartResult.match(/\d\d?.?\D/g);
+
+    for (let i = 0; i < darts.length; i++) {
+        let split = darts[i].match(/(^\d{1,})(S|D|T)(\*|#)?/),
+            score = Math.pow(split[1], bonus[split[2]]) * options[split[3]];
+
+        if (split[3] === '*' && darts[i - 1]) darts[i - 1] *= options['*'];
+
+        darts[i] = score;
+    }
+
+    return darts.reduce((a, b) => a + b);
+}
+
+console.log(solution("1S2D*3T"));
+console.log(solution("1D2S#10S"));
+console.log(solution("1D2S0T"));
+console.log(solution("1S2T3S"));
+console.log(solution("1D#2S*3S"));
+console.log(solution("1T2D3D#"));
+console.log(solution("1D2S3T*"));
+console.log(solution("1S1S10S#"));
