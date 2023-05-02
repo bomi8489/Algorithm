6d8935593f9e0fd5153342be307fb34d5e9a4158
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-02 18:59:39 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,18 @@
+function solution(n, words) {
+    let wordArray = [words[0]];
+    let answer = [0, 0]
+    
+    for(let i = 1; i < words.length; i++) {
+        if(wordArray.indexOf(words[i]) !== -1) {
+            answer = [i%n+1, Math.floor(i/n+1)];
+            break;
+        }
+        else if(wordArray[wordArray.length-1][wordArray[wordArray.length-1].length-1] !== words[i][0]) {
+            answer = [i%n+1, Math.floor(i/n+1)];
+            break;
+        }
+        else wordArray.push(words[i])
+    }
+
+    return answer;
+}
