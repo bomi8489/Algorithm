4eff343ad79b737daa687f12d50a82504d247abe
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-02 18:09:26 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -0,0 +1,6 @@
+function solution(n) {
+    let numOfOne = n.toString(2).split('').filter(data => data === '1').length;
+    for(let answer = n+1; ; answer++) {
+        if(answer.toString(2).split('').filter(data => data === '1').length === numOfOne) return answer;
+    }
+}
