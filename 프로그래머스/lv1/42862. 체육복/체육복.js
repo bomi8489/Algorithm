125c0ee07ed9ec252980d767ca675d335d20614c
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-05 14:33:24 +0900

    [level 1] Title: 체육복, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.js
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -1,24 +1,28 @@
 function solution(n, lost, reserve) {
     let answer = n - lost.length;
-    lost.sort((a, b) => a - b)
-    reserve.sort((a, b) => a - b)
+    lost.sort((a, b) => a - b);
+    reserve.sort((a, b) => a - b);
     
+    // 여벌의 체육복을 가지고 있는 학생 먼저 lost배열과 reserve배열에서 제거
     for(let i = 0; i < reserve.length; i++){
         if(lost.indexOf(reserve[i]) !== -1) {
-            lost.splice(lost.indexOf(reserve[i]), 1)
-            reserve.splice(reserve.indexOf(reserve[i]), 1)
-            i--
-            answer++
+            lost.splice(lost.indexOf(reserve[i]), 1);
+            reserve.splice(reserve.indexOf(reserve[i]), 1);
+            i--;
+            answer++;
         }
     }
+    
     reserve.map(stuNum => {
+        // 여벌의 체육복이 있는 학생 번호-1번의 학생이 체육복이 없는 경우 lost 배열에서 제거
         if(lost.indexOf(stuNum-1) !== -1) {
-            lost.splice(lost.indexOf(stuNum-1), 1)
-            return answer++
+            lost.splice(lost.indexOf(stuNum-1), 1);
+            return answer++;
         }
+        // 여벌의 체육복이 있는 학생 번호+1번의 학생이 체육복이 없는 경우 lost 배열에서 제거
         else if(lost.indexOf(stuNum+1) !== -1) {
-            lost.splice(lost.indexOf(stuNum+1), 1)
-            return answer++
+            lost.splice(lost.indexOf(stuNum+1), 1);
+            return answer++;
         }
     })
     return answer;
