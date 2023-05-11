18961fd4616031cc8abd818fbe3107ccb91fd9f0
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-11 22:05:07 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,18 @@
+function solution(id_list, report, k) {
+    let setReportArray = [...new Set(report)];
+    let reportedUser = Array(id_list.length).fill(0);
+    let receivedMail = Array(id_list.length).fill(0);
+    
+    setReportArray = setReportArray.map(reportedData => reportedData.split(' '));
+    setReportArray.map(reportedDataArray => {
+        reportedUser[id_list.indexOf(reportedDataArray[1])] += 1;
+    });
+    let tmpArray = reportedUser.map((reportedNum, idx) => {
+        if(reportedNum >= k) {
+            setReportArray.map(reportedDataArray => {
+                if(reportedDataArray[1] === id_list[idx]) receivedMail[id_list.indexOf(reportedDataArray[0])]++
+            })
+        }
+    })
+    return receivedMail;
+}
