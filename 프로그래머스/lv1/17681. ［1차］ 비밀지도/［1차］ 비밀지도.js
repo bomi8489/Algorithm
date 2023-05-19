73f71ad4d4a7491d8b66a346d5db88764518c789
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-19 16:37:00 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.29 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,5 +1,7 @@
 function solution(n, arr1, arr2) {
-    let sumArr = arr1.map((data, idx) => (data | arr2[idx]).toString(2).split(''))
-    let lengthCheck = sumArr.map(data => (data.length !== n) ? '0'.repeat(n - data.length)+data.join('') : data.join(''))
+    let sumArr = arr1.map((data, idx) => (data | arr2[idx])
+                          .toString(2).split(''));
+    let lengthCheck = sumArr.map(data => (data.length !== n) ? '0'
+                                 .repeat(n - data.length)+data.join('') : data.join(''))
     return lengthCheck.map(data => data.replace(/1|0/g, num => +num ? '#' : ' '))
 }
