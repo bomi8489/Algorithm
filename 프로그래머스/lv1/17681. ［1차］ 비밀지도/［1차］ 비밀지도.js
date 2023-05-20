80609ce7bafc32e5a36f6d101fe54b7929151827
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-20 17:28:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,7 +1,20 @@
 function solution(n, arr1, arr2) {
-    let sumArr = arr1.map((data, idx) => (data | arr2[idx])
-                          .toString(2).split(''));
-    let lengthCheck = sumArr.map(data => (data.length !== n) ? '0'
-                                 .repeat(n - data.length)+data.join('') : data.join(''))
-    return lengthCheck.map(data => data.replace(/1|0/g, num => +num ? '#' : ' '))
+    let answer = [];
+    let sumArr = [];
+    
+    // arr1과 arr2 or 연산하기
+    for(let i = 0; i < n; i++) {
+        sumArr.push((arr1[i] | arr2[i]).toString(2));
+    }
+    
+    // 위의 연산결과의 배열 길이가 n보다 작을경우 즉, 최상위 비트가 0이라 생략된 경우
+    for(let i = 0; i < sumArr.length; i++) {
+        sumArr[i] = sumArr[i].padStart(n, '0');
+    }
+    
+    // 배열의 요소가 1이면 '#', 0이면 ' '으로 변환
+    for(let i = 0; i < sumArr.length; i++) {
+        answer.push(sumArr[i].replace(/1|0/g, num => +num ? '#' : ' '));
+    } 
+    return answer;
 }
