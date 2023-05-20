e6b1a646aaac6f5e783e31d3458fc2e9e0417aa3
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-20 19:35:14 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.77 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,16 +1,23 @@
 function solution(survey, choices) {
-    const MBTI = {};
-    const types = ["RT","CF","JM","AN"];
-
-    types.forEach((type) =>
-        type.split('').forEach((char) => MBTI[char] = 0)
-    )
-
-    choices.forEach((choice, index) => {
-        const [disagree, agree] = survey[index];
-
-        MBTI[choice > 4 ? agree : disagree] += Math.abs(choice - 4);
-    });
-
-    return types.map(([a, b]) => MBTI[b] > MBTI[a] ? b : a).join("");
+    let answer = 'RCJA';
+    let typeArr = ['RT', 'CF', 'JM', 'AN'];
+    let typeNum = [0, 0, 0, 0];
+    
+    survey.map((data, idx) => {
+        if(choices[idx] > 4) {
+            if(typeArr.includes(data)) typeNum[typeArr.indexOf(data)] += (choices[idx] - 4);
+            else typeNum[typeArr.indexOf(data.split('').reverse().join(''))] -= (choices[idx] - 4);
+        }
+        else {
+            if(typeArr.includes(data)) typeNum[typeArr.indexOf(data)] += (choices[idx] - 4);
+            else typeNum[typeArr.indexOf(data.split('').reverse().join(''))] -= (choices[idx] - 4);
+        }
+    })
+    
+    if(typeNum[0] > 0) answer = answer.replace('R', 'T');
+    if(typeNum[1] > 0) answer = answer.replace('C', 'F');
+    if(typeNum[2] > 0) answer = answer.replace('J', 'M');
+    if(typeNum[3] > 0) answer = answer.replace('A', 'N');
+    
+    return answer;
 }
