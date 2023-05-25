1670810731f0b7099ae2aead52b50fad583cd9bd
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-05-25 18:13:56 +0900

    [level 1] Title: 폰켓몬, Time: 0.24 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,4 +1,4 @@
 function solution(nums) {
     let setNums = [...new Set(nums)];
-    return (nums.length/2 < setNums.length) ? answer = Math.floor(nums.length/2) : answer = setNums.length
+    return (nums.length/2 < setNums.length) ? Math.floor(nums.length/2) : setNums.length
 }
