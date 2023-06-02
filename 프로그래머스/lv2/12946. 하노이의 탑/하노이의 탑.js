3a6a3a1032ef901af95f341f2ca7c997b1ea2eea
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2023-06-02 17:15:02 +0900

    [level 2] Title: 하노이의 탑, Time: 8.65 ms, Memory: 48.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.js
@@ -0,0 +1,23 @@
+function solution(n) {
+    let answer = [];
+    let start = 1;
+    let via = 2;
+    let goal = 3;
+    
+    let move = (start, goal) => {
+        answer.push([start, goal]);
+    }
+    
+    let hanoi = (n, start, goal, via) => {
+        if(n === 1) move(start, goal)
+        else {
+            hanoi(n-1, start, via, goal);
+            move(start, goal);
+            hanoi(n-1, via, goal, start);
+        }
+    }
+    
+    hanoi(n, start, goal, via);
+    
+    return answer;
+}
