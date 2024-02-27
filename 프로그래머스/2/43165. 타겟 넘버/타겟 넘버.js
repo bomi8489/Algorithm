dff5d27e395312b54572a51860378f652b44f1e7
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-02-28 00:27:03 +0900

    [level 2] Title: 타겟 넘버, Time: 1.05 ms, Memory: 35.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,15 +1,15 @@
 function solution(numbers, target) {
     let answer = 0
-    function DFS(sum, depth) {
-        if(depth === numbers.length) {
+    function DFS(sum, index) {
+        if(index === numbers.length) {
             if(sum === target) {
                 answer += 1
             }
             return
         }
 
-        DFS(sum + numbers[depth], depth + 1)
-        DFS(sum - numbers[depth], depth + 1)
+        DFS(sum + numbers[index], index + 1)
+        DFS(sum - numbers[index], index + 1)
     }
     DFS(0, 0)
     return answer
