b59e44033a1be386d4382b589ee30b68607ca7c4
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-02-27 21:39:40 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,8 @@
+function solution(t, p) {
+    let pLength = p.length
+    let answer = t.split('').map((data, idx) => 
+        [...t].splice(idx, pLength).join('')
+    ).filter(data => data.length == pLength)
+    .filter(v => Number(v) <= p).length
+    return answer;
+}
