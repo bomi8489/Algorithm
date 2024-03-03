dc8336152566993028247b2da6b7db0adb70c1a1
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-04 02:29:13 +0900

    [level 2] Title: 피보나치 수, Time: 4.75 ms, Memory: 39 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,7 @@
+function solution(n) {
+    let fibonacci = [0, 1]
+    for(let i = 2; i <= n; i++){
+        fibonacci.push((fibonacci[fibonacci.length-1]+fibonacci[fibonacci.length-2])%1234567)
+    }
+    return fibonacci[n]
+}
