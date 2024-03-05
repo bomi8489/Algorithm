4dc91d8aa50f5b075d88c77f5bd88d0dc1e0fad2
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-06 05:07:20 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,13 @@
+function solution(brown, yellow) {
+    let width = (brown-2)/2
+    let height = 3
+    for (let i = 0; i < brown/2; i++) {
+        if ((width - 2) * (height - 2) === yellow) {
+            break
+        } else {
+            width--
+            height++
+        }
+    }
+    return [width, height]
+}
