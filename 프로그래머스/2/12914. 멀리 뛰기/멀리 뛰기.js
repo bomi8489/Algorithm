37f3594b40520ad0261209418a63c101175229b1
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-08 15:47:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.28 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,5 +1,4 @@
 function solution(n) {
-    let answer = 0
     let fibonacci = [0, 1]
     for(let i = 1; i <= n; i++){
         fibonacci.push((fibonacci[fibonacci.length-1]+fibonacci[fibonacci.length-2])%1234567)
