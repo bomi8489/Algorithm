5fadf65d2fcd64c7d613ef6997171f50e693b58b
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-12 21:22:30 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 3395.21 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,13 +1,12 @@
 function solution(elements) {
     const answerArr = []
-    const elementsArr = (elements.join('')+elements.join('')).split('')
     for (let i = 1; i <= elements.length; i++) {
         for (let j = 0; j < elements.length; j++) {
             let tmpArr = elements.slice(j, i + j)
             if (i + j > elements.length) {
-                tmpArr = [...tmpArr, ...elements.slice(0, j+i-elements.length)]
+                tmpArr = [...tmpArr, ...elements.slice(0, i + j - elements.length)]
             }
-            answerArr.push(tmpArr.reduce((acc, cur) => parseInt(acc) + parseInt(cur), 0))
+            answerArr.push(tmpArr.reduce((acc, cur) => acc + cur, 0))
         }
     }
     
