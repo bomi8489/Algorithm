71500c2bbbb25440d8056229f1843cad8aa43ae9
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-13 22:23:58 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = []
+    while(n > 0) {
+        if (n % 3 !== 0) {
+            let rest = n % 3
+            n = Math.floor(n / 3)
+            answer.push(rest)
+        } else {
+            answer.push(String(Number(solution(n - 1)) + 2))
+            break
+        }
+    }
+    return answer.reverse().join('')
+}
