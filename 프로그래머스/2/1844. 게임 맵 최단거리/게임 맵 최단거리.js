d9684d3477fe8ae75fd723d1aaba530fbe47d021
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-03-26 00:51:51 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.97 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,30 @@
+function solution(maps) {
+    let xLength = maps[0].length
+    let yLength = maps.length
+    let xGoal = xLength - 1
+    let yGoal = yLength - 1
+    
+    let queue = []
+    queue.push([0, 0, 1])
+    
+    while(queue.length) {
+        let [y, x, answer] = queue.shift()
+        
+        if(x < 0 || x >= xLength) continue
+        if(y < 0 || y >= yLength) continue
+        if(maps[y][x] === 0) continue
+        
+        if(x === xGoal && y === yGoal) {
+            return answer
+        }
+        
+        maps[y][x] = 0
+        
+        queue.push([y+1, x, answer+1])
+        queue.push([y, x+1, answer+1])
+        queue.push([y-1, x, answer+1])
+        queue.push([y, x-1, answer+1])
+    }
+    
+    return -1
+}
