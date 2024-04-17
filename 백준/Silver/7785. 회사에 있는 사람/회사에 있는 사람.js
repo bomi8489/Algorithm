e82853992ec6dc1ff84bd80d9938b111f50af303
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-04-18 05:56:58 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 468 ms, Memory: 62800 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.js
@@ -0,0 +1,21 @@
+const input = require('fs')
+    .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+    .toString()
+    .trim()
+    .split("\n")
+    .map(e => {
+      if (e/e === 1) return Number(e)
+      else return e.split(' ')
+    })
+
+let n = input.shift()
+const peopleMap = new Map(input.map(e => [e[0], e[1]]))
+const tmp = []
+
+peopleMap.forEach((_, e) => {
+  if (peopleMap.get(e) === 'leave') peopleMap.delete(e)
+  else tmp.push(e)
+})
+
+tmp.sort().reverse()
+console.log(tmp.join('\n'))
