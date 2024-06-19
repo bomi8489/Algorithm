a765afeb4a67383ec0e8ab3a98dc477688eba512
B._.OMI <66124037+bomi8489@users.noreply.github.com>
2024-06-19 20:50:15 +0900

    [Gold IV] Title: 회사 문화 1, Time: 428 ms, Memory: 92488 KB -BaekjoonHub

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.js
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.js
@@ -10,14 +10,17 @@ const data = input.slice(2, 2 + m).map(e => e.split(' ').map(Number))
 const tree = {}
 const complimentArr = Array(n + 1).fill(0)
 
+// 트리 초기화
 for (let i = 1; i <= n; i++) {
   tree[i] = []
 }
 
+// 입력 값 트리에 채우기
 for (let i = 1; i < n; i++) {
   tree[relationArr[i]].push(i + 1)
 }
 
+// 1번 노드부터 시작해서 자신의 칭찬점수 직속 부하에게 더해주기
 const bfs = (start) => {
   const queue = [start]
   let front = 0
@@ -30,6 +33,7 @@ const bfs = (start) => {
   }
 }
 
+// 각 사원이 받은 칭찬값 각 노드에 더해주기
 for (const [node, compliment] of data) {
   complimentArr[node] += compliment
 }
